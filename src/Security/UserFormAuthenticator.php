c33b706235bb9c686d6760d06892476ffcb3dd60
Freddyede <patouillardfranck3@gmail.com>
2024-01-18 16:20:35 +0100

    Updated after connexion effect
    Signed-off-by: Franck <136079691+Freddyede@users.noreply.github.com>

--- src/Security/UserFormAuthenticator.php
+++ src/Security/UserFormAuthenticator.php
@@ -22,7 +22,7 @@ class UserFormAuthenticator extends AbstractLoginFormAuthenticator
 
     public const LOGIN_ROUTE = 'app_login';
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    public function __construct(private readonly UrlGeneratorInterface $urlGenerator)
     {
     }
 
@@ -47,10 +47,9 @@ class UserFormAuthenticator extends AbstractLoginFormAuthenticator
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
         }
-
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate(match ($token->getUser()->getRoles()['roles']){
+            'ROLE_USER' => 'profil.index'
+        }));
     }
 
     protected function getLoginUrl(Request $request): string
